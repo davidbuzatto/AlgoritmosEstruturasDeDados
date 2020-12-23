a4dbf96cb33ff37053cc102ab8b82e0bae819862
David Buzatto <davidbuzatto@gmail.com>
2020-12-23 14:38:39 -0300

    Implementation of the tests of Prim, Kruskal and Dijkstra algorithms, copying those classes to insert various sout messages to trace their execution.

--- src/aesd/esdc4/algorithms/graph/edgeweighted/tests/TestDirectedDijkstraSPTrace.java
+++ src/aesd/esdc4/algorithms/graph/edgeweighted/tests/TestDirectedDijkstraSPTrace.java
@@ -6,29 +6,28 @@
 package aesd.esdc4.algorithms.graph.edgeweighted.tests;
 
 import aesd.esdc4.ds.implementations.nonlinear.graph.Edge;
-import aesd.esdc4.ds.implementations.nonlinear.graph.EdgeWeightedGraph;
+import aesd.esdc4.ds.implementations.nonlinear.graph.EdgeWeightedDigraph;
 
 /**
- * Teste de uso do algoritmo de Prim na variação lazy usando a classe 
- * LazyPrimMSTTrace para verificar o que está acontecendo.
+ * Teste de uso do algoritmo de Dijkstra classe DirectedDijkstraSPTrace para
+ * verificar o que está acontecendo.
  * 
  * @author Prof. Dr. David Buzatto
  */
-public class TestLazyPrimMST {
+public class TestDirectedDijkstraSPTrace {
     
     public static void main( String[] args ) {
         
-        EdgeWeightedGraph g = new EdgeWeightedGraph( 8 );
+        EdgeWeightedDigraph g = new EdgeWeightedDigraph( 8 );
         g.addEdge( 4, 5, 0.35 );
+        g.addEdge( 5, 4, 0.35 );
         g.addEdge( 4, 7, 0.37 );
         g.addEdge( 5, 7, 0.28 );
-        g.addEdge( 0, 7, 0.16 );
-        g.addEdge( 1, 5, 0.32 );
+        g.addEdge( 7, 5, 0.28 );
+        g.addEdge( 5, 1, 0.32 );
         g.addEdge( 0, 4, 0.38 );
-        g.addEdge( 2, 3, 0.17 );
-        g.addEdge( 1, 7, 0.19 );
         g.addEdge( 0, 2, 0.26 );
-        g.addEdge( 1, 2, 0.36 );
+        g.addEdge( 7, 3, 0.39 );
         g.addEdge( 1, 3, 0.29 );
         g.addEdge( 2, 7, 0.34 );
         g.addEdge( 6, 2, 0.40 );
@@ -37,8 +36,8 @@ public class TestLazyPrimMST {
         g.addEdge( 6, 4, 0.93 );
         System.out.println( g );
         
-        LazyPrimMSTTrace lpMST = new LazyPrimMSTTrace( g );
-        for ( Edge e : lpMST.edges() ) {
+        DirectedDijkstraSPTrace dSP = new DirectedDijkstraSPTrace( g, 0 );
+        for ( Edge e : dSP.pathTo( 0 ) ) {
             System.out.println( e );
         }
         
